2a4b4583e415332d5e0c661608cba334eea3fb30
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-10-13 17:45:35 +0300

    Made it

--- server/seed.py
+++ server/seed.py
@@ -13,28 +13,30 @@ with app.app_context():
     RestaurantPizza.query.delete()
 
     print("Creating restaurants...")
-    shack = Restaurant(name="Karen's Pizza Shack", address='address1')
-    bistro = Restaurant(name="Sanjay's Pizza", address='address2')
-    palace = Restaurant(name="Kiki's Pizza", address='address3')
+    shack = Restaurant(name="Karen's Pizza Shack", address="address1")
+    bistro = Restaurant(name="Sanjay's Pizza", address="address2")
+    palace = Restaurant(name="Kiki's Pizza", address="address3")
     restaurants = [shack, bistro, palace]
+    db.session.add_all(restaurants)
+    db.session.commit()
 
     print("Creating pizzas...")
 
     cheese = Pizza(name="Emma", ingredients="Dough, Tomato Sauce, Cheese")
-    pepperoni = Pizza(
-        name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+    pepperoni = Pizza(name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
     california = Pizza(
-        name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard")
+        name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard"
+    )
     pizzas = [cheese, pepperoni, california]
+    db.session.add_all(pizzas)
+    db.session.commit()
 
     print("Creating RestaurantPizza...")
 
-    pr1 = RestaurantPizza(restaurant=shack, pizza=cheese, price=1)
-    pr2 = RestaurantPizza(restaurant=bistro, pizza=pepperoni, price=4)
-    pr3 = RestaurantPizza(restaurant=palace, pizza=california, price=5)
+    pr1 = RestaurantPizza(restaurant_id=shack.id, pizza_id=cheese.id, price=1)
+    pr2 = RestaurantPizza(restaurant_id=bistro.id, pizza_id=pepperoni.id, price=4)
+    pr3 = RestaurantPizza(restaurant_id=palace.id, pizza_id=california.id, price=5)
     restaurantPizzas = [pr1, pr2, pr3]
-    db.session.add_all(restaurants)
-    db.session.add_all(pizzas)
     db.session.add_all(restaurantPizzas)
     db.session.commit()
 
